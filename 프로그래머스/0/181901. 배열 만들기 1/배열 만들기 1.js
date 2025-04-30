75bc971f97b7884ce50c851e4f19f68e3da5c174
정원빈 (Wonbin Jeong) <wonbin1301@gmail.com>
2025-04-30 09:51:00 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.18 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.js
@@ -1,9 +1,4 @@
 function solution(n, k) {
-    let answer = [];
-    for (i = 1; i <= n; i++) {
-        if (i % k === 0) {
-            answer.push(i);
-        }
-    }
-    return answer.sort((a,b) => a-b);
+    let max = Math.floor(n/k)
+    return [...new Array(max)].map((_, i) => (i + 1) * k)
 }
