e4e0b9cd3158a3e027ffc6847059736d51de0a5b
정원빈 (Wonbin Jeong) <wonbin1301@gmail.com>
2025-05-01 11:07:25 +0900

    [level 0] Title: 배열 만들기 4, Time: 16.19 ms, Memory: 39.8 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -1,20 +1,8 @@
 function solution(arr) {
     let stk = [];
-    let i = 0;
-    while (i < arr.length) {
-        if(stk.length === 0) {
-            stk.push(arr[i]);
-            i += 1;
-        }
-        // else if (stk[stk.length-1] < arr[i]) { 구형 브라우저 지원이 필요할 경우
-        else if (stk.at(-1) < arr[i]) { // 신형 브라우저 
-            stk.push(arr[i]);
-            i += 1;
-        }
-        else {
-            stk.pop();
-        }
-        
+    for (const v of arr) {
+        while (stk.at(-1) >= v) stk.pop();
+        stk.push(v);
     }
     return stk;
 }
